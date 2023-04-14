4c99d038e316be3dcc830b1f28be5ce88775ee4b
tladudtn <tladudtn7598@gmail.com>
2023-04-14 06:07:58 +0000

    04/14

--- ban.sh
+++ ban.sh
@@ -1,18 +1,74 @@
 #!/bin/bash
 
+# 처음에 프로그램 실행하면, 필요한 필수 패키지 확인한 후 국가별 IP 파일들 받기 
+# 패키지 확인 -> 국가별 IP 범위 받기 -> 국가 선택후 정책 허용/차단 결정
+
 rules_dir="./rules"
 LOCAL=""
 FILE_NAME=""
 FILE_LINE="0"
+ 
+function chk_package() {
+if [ -z $(which iptables) ]; then
+        echo "[X] iptables not found"
+        exit 1
+    elif [ -z $(which curl) ]; then
+        echo "[X] curl not found"
+        exit 1
+    elif [ -z $(which wc) ]; then
+        echo "[X] wc not found"
+        exit 1
+else {
+    echo "all package already installed"   
+}
+fi
+   
+}
 
+select_menu() {
+    echo " [    MENU    ]"
+    echo "1. Select Coutury & BAN IP"
+    echo "2. Select Coutury & UNBAN IP"  
+    echo -e "3. exit"
+    echo -n "[1~3]: " 
+    read MENU
+
+    if [ $MENU -eq 1 ] || [ $MENU -eq 2 ]; then   
+        clear
+        select_contury
+        elif [ $MENU -eq 3 ]; then 
+            echo 3
+    else   
+        select_menu
+    fi
+}
+
+function ban_or_unban() {
+    echo " [ BAN or UNBAN ]"
+    echo "1. start ban ip"
+    #echo -e "2. start unban ip"
+    echo -n "[1~2]: "
+    read MENU
+
+    if [ $MENU -eq 1 ]; then
+        ban_ip
+        elif [ $MENU -eq 2 ]; then
+            unban_ip
+    else {
+        clear
+        ban_or_unban
+    } 
+    fi
+}
 
 function file_download() {
 http_code=$(curl -w '%{http_code}\n' -o ${rules_dir}/ipdeny_${LOCAL}.zone ipdeny.com/ipblocks/data/countries/${LOCAL}.zone )
     if [ $http_code -eq 200 ]; then
         echo "Download Complete!"
         FILE_NAME="./${rules_dir}/ipdeny_${LOCAL}.zone"
+        clear
+        ban_or_unban
         elif [ $http_code -eq 404 ]; then
-        	# 좀더 깔쌈한 방법이 있을거 같은데..
             echo "404 File Not Found"
             rm -rf "${rules_dir}/ipdeny_.zone"
     else
@@ -20,8 +76,18 @@ http_code=$(curl -w '%{http_code}\n' -o ${rules_dir}/ipdeny_${LOCAL}.zone ipdeny
     fi
 }
 
-
-# 예외처리 안함 일단 작동만 가능하게끔
+function unban_ip {
+    FILE_LINE=$(wc -l ./${rules_dir}/ipdeny_${LOCAL}.zone | grep -o '^[1-9]*')
+    for((var=1; var<=$FILE_LINE; var++));
+    do
+        #echo $var
+        IP_SUBNET=$(sed -n -e  "$var"p ./"$FILE_NAME")
+        
+        sudo iptables -A INPUT -t filter -s $IP_SUBNET -j DROP 
+        echo UNBAN $IP_SUBNET COMPLETE
+    done
+}
+# :X
 function ban_ip { 
 FILE_LINE=$(wc -l ./${rules_dir}/ipdeny_${LOCAL}.zone | grep -o '^[1-9]*')
     
@@ -34,7 +100,7 @@ FILE_LINE=$(wc -l ./${rules_dir}/ipdeny_${LOCAL}.zone | grep -o '^[1-9]*')
         echo BAN $IP_SUBNET COMPLETE
     done
 }
-#chk_pkg
+
 
 function select_contury {
     echo "# Select Contury "
@@ -45,27 +111,33 @@ function select_contury {
     if [ $SELECT_LOCAL -eq 1 ]; then
             echo "SELECT CN"
             LOCAL="cn"
+            file_download "$LOCAL"
             elif [ $SELECT_LOCAL -eq 2 ]; then
                 echo "SELECT RU"
                 LOCAL=ru
+                file_download "$LOCAL"
             elif [ $SELECT_LOCAL -eq 3 ]; then
                 echo "SELECT HK"
                 LOCAL=hk
+                file_download "$LOCAL"
             elif [ $SELECT_LOCAL -eq 4 ]; then
                 echo "SELECT TW"
                 LOCAL=tw
+                file_download "$LOCAL"
             elif [ $SELECT_LOCAL -eq 5 ]; then
                 echo "SELECT ID"
                 LOCAL=id
+                file_download "$LOCAL"
         else
-            echo "out of range"
+            select_contury
     fi
 }
 # 해당 파일 줄수 출력
 
 # echo $FILE_LINE
 
-select_contury
-file_download "$LOCAL"
-ban_ip
+#select_contury
+
+chk_package
+select_menu
 echo EOF
